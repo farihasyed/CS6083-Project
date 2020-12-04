bf50a00a46a40f6128271c26095b271bdc90e971
Fariha Syed <fs1688@nyu.edu>
2020-12-03 22:41:31 -0500

    updated database name to match that of gauss server

--- data/create_database.sh
+++ data/create_database.sh
@@ -1,7 +1,8 @@
 psql -U fs1688 -d fs1688 -a -f schema.sql
 
-cat zip_codes_is_in.csv | psql -U fs1688 -d fs1688 -h localhost -p 5432 -c "copy Zip_Codes_Is_In from stdin csv header"
-cat train_lines.csv | psql -U fs1688 -d fs1688 -h localhost -p 5432 -c "copy Train_Lines from stdin csv header"
-cat train_stations_have.csv | psql -U fs1688 -d fs1688 -h localhost -p 5432 -c "copy Train_Stations_Have from stdin csv header"
-cat stops_at.csv | psql -U fs1688 -d fs1688 -h localhost -p 5432 -c "copy Stops_At from stdin csv header"
+cat zip_codes_is_in.csv | psql -U fs1688 -d fs1688-db -h localhost -p 5432 -c "copy Zip_Codes_Is_In from stdin csv header"
+cat covid_casualties.csv | psql -U fs1688 -d fs1688-db -h localhost -p 5432 -c "copy COVID_Casualties_Are_In from stdin csv header"
+cat train_lines.csv | psql -U fs1688 -d fs1688-db -h localhost -p 5432 -c "copy Train_Lines from stdin csv header"
+cat train_stations_have.csv | psql -U fs1688 -d fs1688-db -h localhost -p 5432 -c "copy Train_Stations_Have from stdin csv header"
+cat stops_at.csv | psql -U fs1688 -d fs1688-db -h localhost -p 5432 -c "copy Stops_At from stdin csv header"
 
