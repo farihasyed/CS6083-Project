87b0f38c2d133d1f25bb641bac04fa7ff725ec24
Fariha Syed <fs1688@nyu.edu>
2020-12-05 16:46:31 -0500

    minor changes

--- src/project.py
+++ src/project.py
@@ -5,7 +5,7 @@ from configparser import ConfigParser
 
 
 @st.cache
-def get_config(filename='project/database.ini', section='postgresql'):
+def get_config(filename='database.ini', section='postgresql'):
     parser = ConfigParser()
     parser.read(filename)
     return {k: v for k, v in parser.items(section)}
