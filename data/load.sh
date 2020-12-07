5d0d59cd688ec54841b2a6819787bfaa0a913878
Fariha Syed <fs1688@nyu.edu>
2020-12-06 20:04:17 -0500

    meh

--- data/load.sh
+++ data/load.sh
@@ -1,4 +1,4 @@
-# psql -U fs1688 -d fs1688_project -a -f data/load.sql
+psql -U fs1688 -d fs1688_project -a -f data/load.sql
 cat data/zip_codes_is_in.csv | psql -U fs1688 -d fs1688_project -h localhost -p 5432 -c "copy Zip_Codes_Is_In from stdin csv header"
 cat data/covid_casualties.csv | psql -U fs1688 -d fs1688_project -h localhost -p 5432 -c "copy COVID_Casualties_Are_In from stdin csv header"
 cat data/train_lines.csv | psql -U fs1688 -d fs1688_project -h localhost -p 5432 -c "copy Train_Lines from stdin csv header"
